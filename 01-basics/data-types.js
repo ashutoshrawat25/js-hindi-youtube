ff916f0a0fc1516c55ed9a564edddd6e3c50b2e7
ashutoshrawat25 <ashutosh.rawat4@gmail.com>
2023-10-16 14:04:11 +0530

    data-types

---
+++ 01-basics/data-types.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all the js ccode as newer version
+
+// alert(3 + 3); // we are using node js not browser
+
+console.log(3 + 3); // code redablity should be high
+
+let name = "ashu"; //string
+let num = 45; //number;
+let isLoggedIn = true; //boolean;
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
+//primitive data types
+
+//number = 2 to power of 53
+// bigint
+//string => ""
+//boolean => true/false
+//null => standalone value /empty
+//undefined => value not assigned
+//symbol => uniqueness
+
+//object
